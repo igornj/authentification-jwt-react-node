7a500be8ba9b956ea7d7a10c46c666ff9d89b3d4
Igor Nunes <igornj@hotmail.com>
2022-02-02 14:54:55 -0300

    f c

---
+++ Server/server.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./model/User');
+dotenv.config();
+
+
+//Connect to DB
+mongoose
+    .connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        //Checking if the user is already in the database
+        try {
+            const usernameExist = await User.findOne({ username: username });
+            if (usernameExist) {
+                console.log('usuário ja existe');
+                return res.status(400).send('username already exists');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        //CREATE A NEW USER
+        const user = new User({
+            username: username,
+            password: hashPassword
+        });
+
+        try {
+            const savedUser = await user.save();
+            res.send({ user: savedUser });
+            console.log('user registered!')
+        } catch (e) {
+            res.status(400).send(e);
+            console.log(e);
+            console.log('data not sent')
+        }
+    }
+
+})
+
+app.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //user exist
+    const usernameExist = await User.findOne({ username: username });
+    if (!usernameExist) {
+        res.json({ auth: false, message: "Incorrect username" });
+        return res.status(400);
+    }
+
+    //Password is correct
+    const validPass = await bcrypt.compare(password, usernameExist.password);
+    if (!validPass) {
+        res.json({ auth: false, message: "Incorrect password" });
+        return res.status(400);
+    }
+
+    if (usernameExist) {
+        //Create and assign a token
+        const token = jwt.sign({ _id: usernameExist._id }, process.env.TOKEN_SECRET, {
+            expiresIn: 300,
+        });
+        res.json({ auth: true, token: token, user: usernameExist });
+    } else {
+        res.json({ auth: false, message: "User doesn't exist" });
+    }
+
+    console.log('user logged in!')
+})
+
+//middleware to check the jwt token created
+const verifyJwt = (req, res, next) => {
+    const token = req.headers["x-access-token"];
+
+    if (!token) {
+        res.send("We need a token to check if you are authorized")
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        if (err) res.json({ auth: false, message: "You failed to authenticate" });
+
+        req.userId = decoded._id;
+        next();
+    });
+}
+
+app.get('/authenticated', verifyJwt, (req, res) => {
+    res.send('You are authenticated');
+})
+
+
+app.listen(3001, () => {
+    console.log(`Server running on port: 3001`);
+});
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import react, { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [loginStatus, setLoginStatus] = useState(false);
+
+  console.log(usernameReg, passwordReg);
+
+  const handleRegister = () => {
+    axios.post('http://localhost:3001/register', {
+      username: usernameReg,
+      password: passwordReg,
+    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch(error => console.log(error));
+  }
+
+
+  const handleLogin = () => {
+    axios.post('http://localhost:3001/login', {
+      username: username,
+      password: password,
+    })
+      .then((response) => {
+        localStorage.setItem('token', response.data.token);
+        setLoginStatus(true);
+      })
+      .catch(error => {
+        console.log(error)
+        setLoginStatus(false);
+      });
+  }
+
+  const isUserAuthenticated = () => {
+    axios.get('http://localhost:3001/authenticated', {
+      headers: {
+        'x-access-token': localStorage.getItem('token'),
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center', rowGap: "0.8rem", marginTop: "5rem" }}>
+        <h1>Register</h1>
+        <label>Username:</label>
+        <input type="text" placeholder="user" onChange={(e) => setUsernameReg(e.target.value)} />
+
+        <label>Senha:</label>
+        <input type="password" placeholder="password" onChange={(e) => setPasswordReg(e.target.value)} />
+        <button onClick={handleRegister} type="submit">Register</button>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center', rowGap: "0.8rem", marginTop: "4rem" }}>
+        <h1>LOGIN</h1>
+        <label>Username:</label>
+        <input type="text" placeholder="user" onChange={(e) => setUsername(e.target.value)} />
+
+        <label>Senha:</label>
+        <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit" onClick={handleLogin} >Login</button >
+
+        {loginStatus && (
+          <button onClick={isUserAuthenticated} style={{ background: 'red', cursor: 'pointer', color: 'white', marginTop: '2rem', width: '50%', fontSize: '1.5rem', border: 'none', borderRadius: '10px', padding: '15px 20px' }}>Check if you are authenticated</button>
+        )}
+      </div>
+
+
+
     </div>
+
   );
 }
 
